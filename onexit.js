829cd7d34f0483911ba0dd5854fca7cbd21074bf
stefan <stefan@crystalmath.co>
2016-09-03 15:35:58 +0200

    Removed JQuery as dependency. Asynchonously load stylesheet and popup html.

--- onexit.js
+++ onexit.js
@@ -7,18 +7,54 @@
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 (function() {
-    $(document)
-        .on('click', 'button', function () {
-            document.getElementById('goodbye').classList.add("hide");
-            document.getElementById('shadow-box').classList.add("hide");
-            show = false;
-        });
-    $('.body').on('mouseleave', function (e) {
-        if (e.pageY - window.pageYOffset <= 0 && show) {
-            var goodbye = document.getElementById('goodbye');
-            goodbye.classList.remove("hide");
-            document.getElementById('shadow-box').classList.remove("hide");
-            show = false;
+    /**
+     * Add css to head
+     */
+    var link = document.createElement('link');
+    link.href = 'onexit.css';
+    link.type = 'text/css';
+    link.rel = 'stylesheet';
+    link.media = 'screen';
+
+    document.head.appendChild(link);
+
+    /**
+     * Async. get html for pop up
+     */
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "onexit.html", true);
+    xhr.onload = function (e) {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                console.log(xhr);
+                var onexit = document.createElement('div');
+                onexit.setAttribute('class', 'hide');
+                onexit.setAttribute('id', 'onexit');
+                onexit.innerHTML = xhr.response;
+                document.body.appendChild(onexit);
+                /**
+                 * Event handling
+                 */
+                var seen = false;
+                document.documentElement.addEventListener('mouseleave', function (e) {
+                    if (e.pageY - window.pageYOffset <= 0 && !seen) {
+                        onexit.classList.remove("hide");
+                        seen = true;
+                    }
+                });
+
+                var closeBtn = document.getElementById('onexit-close-btn');
+                closeBtn.addEventListener('click', function () {
+                    onexit.classList.add("hide");
+                });
+            } else {
+                console.error(xhr.statusText);
+            }
         }
-    });
+    };
+    xhr.onerror = function (e) {
+        console.error(xhr.statusText);
+    };
+    xhr.send(null);
+
 })();
